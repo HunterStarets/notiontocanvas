0beb5e097ada5a4e2266499e3e445ba3fd4c9f47
Hunter1580 <hunter.starets@gmail.com>
2023-03-20 22:45:43 -0400

    test

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function getCareers() {
   // Set up the Canvas API endpoint and your API key
   const API_URL =
-    "https://cors-anywhere.herokuapp.com/https://your-canvas-instance/api/v1";
+    "https://cors-anywhere.herokuapp.com/https://uncc.instructure.com/api/v1";
   const API_KEY =
     "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
 

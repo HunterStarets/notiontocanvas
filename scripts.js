de0d1dd57bcdc371cd636cc503a660a5041da561
Hunter1580 <hunter.starets@gmail.com>
2023-03-20 22:42:56 -0400

    Update

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
-// Set up the Canvas API endpoint and your API key
-const API_URL =
-  "https://cors-anywhere.herokuapp.com/https://your-canvas-instance/api/v1";
-const API_KEY =
-  "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
+function getCareers() {
+  // Set up the Canvas API endpoint and your API key
+  const API_URL =
+    "https://cors-anywhere.herokuapp.com/https://your-canvas-instance/api/v1";
+  const API_KEY =
+    "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
 
-// Set up the course ID for which you want to retrieve assignments
-const COURSE_ID = "190605";
+  // Set up the course ID for which you want to retrieve assignments
+  const COURSE_ID = "190605";
 
-// Get the assignments for the course using the Canvas API
-fetch(`${API_URL}/courses/${COURSE_ID}/assignments`, {
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-  },
-})
-  .then((response) => response.json())
-  .then((assignments) => {
-    // Do something with the assignments data
-    console.log(assignments);
+  // Get the assignments for the course using the Canvas API
+  fetch(`${API_URL}/courses/${COURSE_ID}/assignments`, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+    },
   })
-  .catch((error) => console.error(error));
+    .then((response) => response.json())
+    .then((assignments) => {
+      // Do something with the assignments data
+      console.log(assignments);
+    })
+    .catch((error) => console.error(error));
+}

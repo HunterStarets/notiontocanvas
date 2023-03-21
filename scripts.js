500e72a13e1ace08d4784a245c77d8f561840a20
Hunter1580 <hunter.starets@gmail.com>
2023-03-20 23:11:00 -0400

    Course Names and ID's

--- scripts.js
+++ scripts.js
@@ -1,12 +1,19 @@
-function getCareers() {
+const courseIds = ["189387", "189166", "190605", "187783", "190687", "188090"];
+const courseNames = [
+  "ITIS-3135",
+  "ITIS-3310",
+  "ITSC-2600",
+  "KNES-1231",
+  "LBST-2301",
+  "MATH-2164",
+];
+getAssignments(courseIds[2]);
+function getAssignments(COURSE_ID) {
   // Set up the Canvas API endpoint and your API key
   const API_URL =
     "https://cors-anywhere.herokuapp.com/https://uncc.instructure.com/api/v1";
   const API_KEY =
     "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
-
-  // Set up the course ID for which you want to retrieve assignments
-  const COURSE_ID = "190605";
 
   // Get the assignments for the course using the Canvas API
   fetch(`${API_URL}/courses/${COURSE_ID}/assignments?per_page=100`, {
@@ -22,4 +29,3 @@ function getCareers() {
     })
     .catch((error) => console.error(error));
 }
-getCareers();

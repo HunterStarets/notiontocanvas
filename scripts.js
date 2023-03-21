aa82efa16efebe113c95fd01d3b7dddc7d9f132e
Hunter1580 <hunter.starets@gmail.com>
2023-03-21 12:12:53 -0400

    GetDatabase

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ const courseNames = [
   "LBST-2301",
   "MATH-2164",
 ];
-getAssignments(courseIds[0]);
 function getAssignments(COURSE_ID) {
   // Set up the Canvas API endpoint and your API key
   const API_URL =
@@ -28,4 +27,54 @@ function getAssignments(COURSE_ID) {
       console.log(assignments);
     })
     .catch((error) => console.error(error));
+}
+/*
+    Important variables 
+        "due_at", 
+        "due_date_required", 
+        "has_submitted_submissions", 
+        "html_url", 
+        "lock_at"
+        "lock_info.lock_at", 
+        "locked_for_user", 
+        "name", 
+        "omit_from_final_grade", 
+        "submission_types",
+        "unlock_at"
+
+const { Client } = require('@notionhq/client');
+
+// Initialize a new Notion client with your API key
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+// The ID of your database
+const databaseId = 'YOUR_DATABASE_ID';
+
+// Retrieve all the assignments in the database
+const response = await notion.databases.query({
+  database_id: databaseId,
+});
+
+// Log the results
+console.log(response.results);
+*/
+
+const NOTION_API_KEY = secret_qexxFPpXBEejliAZ0l8cAEUL3U4QWEQYolqXLg7JbmF;
+getDatabases();
+function getDatabases() {
+  const request = require("request");
+
+  const options = {
+    url: "https://api.notion.com/databases",
+    headers: {
+      "Notion-Version": "2022-03-16",
+      Authorization: "Bearer " + NOTION_API_KEY,
+    },
+  };
+
+  request.get(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+  console.log("Outout");
 }

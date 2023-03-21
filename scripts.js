92d9d9177d6e57c5c0e5e16538f3fe7f84da8601
Hunter1580 <hunter.starets@gmail.com>
2023-03-20 22:18:21 -0400

    async function

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
-const canvasBaseUrl = "https://uncc.instructure.com/api/v1";
-const canvasApiKey =
-  "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
-const courseId = "190605"; //2600 ID
+async function getAssignments() {
+  const canvasBaseUrl = "https://uncc.instructure.com/api/v1";
+  const canvasApiKey =
+    "7301~kT6q0x1iBLQ0oDo0c3nsJHHWbDawgLN76MK0Pj2GckJflPBOCnk2aSBX0b8LY7HI";
+  const courseId = "190605"; //2600 ID
 
-// Retrieve a list of all the assignments for the given course
-const assignmentsUrl = `${canvasBaseUrl}/courses/${courseId}/assignments`;
-const response = await fetch(assignmentsUrl, {
-  headers: { Authorization: `Bearer ${canvasApiKey}` },
-});
-const assignments = await response.json();
+  // Retrieve a list of all the assignments for the given course
+  const assignmentsUrl = `${canvasBaseUrl}/courses/${courseId}/assignments`;
+  const response = await fetch(assignmentsUrl, {
+    headers: { Authorization: `Bearer ${canvasApiKey}` },
+  });
+  const assignments = await response.json();
 
-console.log(assignments);
+  console.log(assignments);
+}
